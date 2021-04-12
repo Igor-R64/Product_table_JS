f3c18077e902c3eb845288c21e56f0073aebf143
Igor <reshetnikovia@outlook.com>
2021-04-13 00:11:26 +0400

    add input

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
-const tab = document.querySelector('.table');
+const tableLauout = ``;
 
-const tr = document.createElement('tr');
+const elements = [
+  {
+    id: 1,
+    count: 9,
+    title: "Цитрамон",
+    price: 50,
+  },
+  {
+    id: 2,
+    count: 5,
+    title: "Аспирин",
+    price: 60,
+  },
+  {
+    id: 3,
+    count: 3,
+    title: "теанин",
+    price: 160,
+  },
+  {
+    id: 4,
+    count: 1,
+    title: "Метамфетамин",
+    price: 1160,
+  },
+];
 
-tr.innerHTML = '<td>1</td><td>2</td><td>3</td>';
+const button = (id) => `<button id = ${id} class="btn btn-primary" style="width: 80px;"> Edit </button>`;
+const delbutton = () => `<button class="btn btn-danger">Delete</button>`;
 
-table.append(tr);
+const makeRow = ({ id, title, price }) => `
+<tr>
+    <td>${title}</td><td>${price}</td><td class="d-flex justify-content-evenly">${button(id)} ${delbutton()} </td>
+  </tr>
+`;
+
+class Table {
+  constructor(elements, idElemToRenderWithin) {
+    this.elements = elements;
+    this.idElemToRenderWithin = idElemToRenderWithin;
+  }
+
+  initialize() {
+    let x = document.getElementById(this.idElemToRenderWithin);
+
+    x.insertAdjacentHTML("afterbegin", this.render(this.elements));
+  }
+
+  render(elements) {
+    let resultHtml = "";
+
+    elements.forEach((el) => {
+      resultHtml = resultHtml + makeRow(el);
+    });
+
+    return `<div class="col-2">${delbutton()}</div>
+    <div class="col-sm-5">${delbutton()} <input type="text" class="form-control" placeholder="City" aria-label="City">
+
+  </div> <table>${resultHtml}</table>`;
+  }
+}
+
+const table = new Table(elements, "table");
+
+table.initialize();

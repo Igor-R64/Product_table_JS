9c5bca4ff8219aac17d43d4119ed257dd9d1d1fa
Igor <reshetnikovia@outlook.com>
2021-04-14 00:12:51 +0400

    add searchline

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const tableLauout = ``;
 
-const elements = [
-  {
+const elements = [{
     id: 1,
     count: 9,
     title: "Цитрамон",
@@ -27,12 +26,28 @@ const elements = [
   },
 ];
 
-const button = (id) => `<button id = ${id} class="btn btn-primary" style="width: 80px;"> Edit </button>`;
-const delbutton = () => `<button class="btn btn-danger">Delete</button>`;
+const button = (id) => `<button id = ${id} class="btn btn-primary" style="width: 100px;"> Edit </button>`;
+const delbutton = () => `<button class="btn btn-danger" style="width: 100px;">Delete</button>`;
+const searchbutton = () => `<button class="btn btn-info" style="width: 100px;"> Search </button>`;
+const addbutton = () => `<button class="btn btn-primary" style="width: 100px;"> Add New </button>`;
 
-const makeRow = ({ id, title, price }) => `
+const searchline = () => `<div class="d-flex justify-content-evenly p-4">
+<input type="text" class="form-control" placeholder="Search..." aria-label="City" style="width: 70%;">
+${searchbutton()} ${addbutton()}
+</div>`;
+
+const makeRow = ({
+  id,
+  count,
+  title,
+  price
+}) => `
 <tr>
-    <td>${title}</td><td>${price}</td><td class="d-flex justify-content-evenly">${button(id)} ${delbutton()} </td>
+    <td class="d-flex justify-content-between">${title}
+    <span class="badge rounded-pill bg-secondary">${count}</span>
+    </td>
+    <td>${price}</td>
+    <td class="d-flex justify-content-evenly">${button(id)} ${delbutton()} </td>
   </tr>
 `;
 
@@ -55,10 +70,24 @@ class Table {
       resultHtml = resultHtml + makeRow(el);
     });
 
-    return `<div class="d-flex justify-content-evenly p-4">
-    <input type="text" class="form-control" placeholder="City" aria-label="City" style="width: 50%;">${delbutton()} ${delbutton()}
-    </div>
- <table>${resultHtml}</table>`;
+    return `
+    <div class = "container">
+      <div class = "row">
+        <div class = "col-2"> </div>
+          <div class = "col-8">
+    ${searchline()}
+              <table class="table table-bordered">
+              <tr>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Action</th>
+  </tr>             
+   ${resultHtml}
+              </table>
+          </div>
+        </div>
+    </div>;
+ `;
   }
 }
 

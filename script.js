5fb083d6641ce88cc95dd772bafce0f4702c4bcb
Igor <reshetnikovia@outlook.com>
2021-04-22 10:51:48 +0400

    add templates

--- script.js
+++ script.js
@@ -1,64 +1,7 @@
 
-const elements = [{
-  id: 1,
-  count: 9,
-  title: "Цитрамон",
-  price: 5000000,
-},
-{
-  id: 2,
-  count: 5,
-  title: "Аспирин",
-  price: 60,
-},
-{
-  id: 3,
-  count: 3,
-  title: "теанин",
-  price: 160,
-},
-{
-  id: 4,
-  count: 1,
-  title: "Метамфетамин",
-  price: 1160,
-},
-];
-
-const button = (id) => `<button data-action="edit" data-id = ${id} class="btn btn-primary" style="width: 100px;"> Edit </button>`;
-const delbutton = (id) => `<button data-action="delete" data-id = ${id} class="btn btn-danger" style="width: 100px;">Delete</button>`;
-const searchbutton = () => `<button data-action="search" class="btn btn-info" style="width: 100px;"> Search </button>`;
-const addbutton = () => `<button data-action="add" class="btn btn-primary" style="width: 100px;"> Add New </button>`;
-
-
-
-const searchline = () => `<div class="d-flex justify-content-evenly p-4">
-<input type="text" class="form-control" placeholder="Search..." aria-label="City" style="width: 70%;">
-${searchbutton()} ${addbutton()}
-</div>`;
-
-const elem = document.getElementById("1");
+// const elem = document.getElementById("1");
     
-elem.style.backgroundColor = 'blue';
-
-
-
-
-
-const makeRow = ({
-  id,
-  count,
-  title,
-  price
-}) => `
-<tr>
-    <td><div class="d-flex justify-content-between" style="heigt: 100%;">${title}
-    <span class="badge rounded-pill bg-secondary">${count}</span>
-    </div></td>
-    <td class="align-middle">${price}</td>
-    <td><div class="d-flex justify-content-evenly">${button(id)} ${delbutton(id)} </div></td>
-  </tr>
-`;
+// elem.style.backgroundColor = 'blue';
 
 class Table {
   constructor(elements, idElemToRenderWithin) {
@@ -133,7 +76,9 @@ class Table {
       this.sortingOrder.orderByName = this.sortingOrder.orderByName === 'asc' ? 'desc' : 'asc';
       
       this.cleaning();
+
       this.render();
+      
       console.log(this.elements);
     }
   
---
+++ templates.js
@@ -0,0 +1,25 @@
+const button = (id) => `<button data-action="edit" data-id = ${id} class="btn btn-primary" style="width: 100px;"> Edit </button>`;
+const delbutton = (id) => `<button data-action="delete" data-id = ${id} class="btn btn-danger" style="width: 100px;">Delete</button>`;
+const searchbutton = () => `<button data-action="search" class="btn btn-info" style="width: 100px;"> Search </button>`;
+const addbutton = () => `<button data-action="add" class="btn btn-primary" style="width: 100px;"> Add New </button>`;
+
+const searchline = () => `<div class="d-flex justify-content-evenly p-4">
+<input type="text" class="form-control" placeholder="Search..." aria-label="City" style="width: 70%;">
+${searchbutton()} ${addbutton()}
+</div>`;
+
+
+const makeRow = ({
+    id,
+    count,
+    title,
+    price
+  }) => `
+  <tr>
+      <td><div class="d-flex justify-content-between" style="heigt: 100%;">${title}
+      <span class="badge rounded-pill bg-secondary">${count}</span>
+      </div></td>
+      <td class="align-middle">${price}</td>
+      <td><div class="d-flex justify-content-evenly">${button(id)} ${delbutton(id)} </div></td>
+    </tr>
+  `;

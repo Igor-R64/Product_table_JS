6889ed11cdbdc0df7f42f7067bdc929dfe2327c8
Igor <reshetnikovia@outlook.com>
2021-05-14 14:42:38 +0400

    fix style searchinput

--- templates.js
+++ templates.js
@@ -3,7 +3,7 @@ const delbutton = (id) => `<button data-action="delete" data-id = ${id} class="b
 const searchbutton = () => `<button data-action="search" class="btn btn-outline-success" style="width: 100px;"> Search </button>`;
 const addbutton = () => `<button data-action="add" class="btn btn-outline-warning" style="width: 100px;"> Add New </button>`;
 
-const searchline = () => `<div class="d-flex justify-content-evenly p-4">
+const searchline = () => `<div class="d-flex justify-content-between p-4">
 <input id="searchinput" type="text" class="form-control" placeholder="Search..." aria-label="City" style="width: 70%;">
 ${searchbutton()} ${addbutton()}
 </div>`;
